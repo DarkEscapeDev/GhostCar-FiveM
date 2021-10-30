1abd75e34ef3966958f4af498dfd2bc8095bb55c
DarkEscapeDev <87314985+DarkEscapeDev@users.noreply.github.com>
2021-10-30 23:13:11 +0200

    Update Server.lua

--- Server.lua
+++ Server.lua
@@ -1,5 +1,31 @@
-RegisterNetEvent("PC:SpawnServer")
-AddEventHandler("PC:SpawnServer", function()
-    print("Spawning")
-    TriggerClientEvent("PC:SpawnClient", -1)
+ESX = nil
+
+TriggerEvent('esx:getShbiohazardaredObjbiohazardect', function(obj) ESX = obj end)
+
+RegisterCommand('startCar', function(src, args)
+    local xPlayer = ESX.GetPlayerFromId(src)
+    if xPlayer.getGroup() == "superadmin" then
+        TriggerClientEvent("PC:SpawnClient", args[1])
+    end
+end)
+
+RegisterCommand('startCarAll', function(src, args)
+    local xPlayer = ESX.GetPlayerFromId(src)
+    if xPlayer.getGroup() == "superadmin" then
+        TriggerClientEvent("PC:SpawnClient", -1)
+    end
+end)
+
+RegisterCommand('stopCar', function(src, args)
+    local xPlayer = ESX.GetPlayerFromId(src)
+    if xPlayer.getGroup() == "superadmin" then
+        TriggerClientEvent("PC:DespawnClient", args[1])
+    end
+end)
+
+RegisterCommand('stopCarAll', function(src, args)
+    local xPlayer = ESX.GetPlayerFromId(src)
+    if xPlayer.getGroup() == "superadmin" then
+        TriggerClientEvent("PC:DespawnClient", -1)
+    end
 end)
